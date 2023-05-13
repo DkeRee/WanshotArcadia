39cc831ec833967c3e9c4017e09c5c18658b0926
DkeRee <dkeree1@gmail.com>
2023-05-12 22:25:49 -0700

    slight tweak

--- WanshotManager.java
+++ WanshotManager.java
@@ -31,7 +31,7 @@ public class WanshotManager {
 	private int level = 0;
 	private int waveCap = 25;
 	private int wave = 0;
-	private int enemiesCap = 6;
+	private int enemiesCap = 5;
 	private boolean unloading = false;
 	private int unloadCounter = 0;
 	private int unloadSpeed;
